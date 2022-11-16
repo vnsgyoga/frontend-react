c8175cc87007eb9ad6e667e11ab9b39b312ae169
lawrence-le <lawrencele1991@gmail.com>
2022-11-16 12:05:24 +0700

    Testing @apply directive for className

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
       <BrowserRouter>
         <div className="flex relative">
           {/* Navbar */}
-          <div className="dark:bg-main-bg bg-main-bg min-h-screen w-full md:ml-72">
+          <div className="dark:bg-main-bg bg-main-bg min-h-screen w-full">
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -3,7 +3,51 @@ import React from "react"
 type Props = {}
 
 const Orders = (props: Props) => {
-  return <div>Orders</div>
+  return (
+    <div className="mt-8">
+      <h1 className="text-gray-700 text-3xl font-bold pb-2 my-12 border-b border-gray-200">
+        Testing @apply directive for className
+      </h1>
+      <main className="flex max-w-4xl">
+        <div className="card">
+          <div className="image bg-teal-200">This is imaginary image</div>
+          <div className="m-4">
+            <span className="font-bold text-gray-500">
+              Grilled Chicken Salad
+            </span>
+            <span className="block text-gray-500 text-sm">Recipe by Unkle</span>
+          </div>
+          <div className="badge">
+            <span>25 mins</span>
+          </div>
+        </div>
+        <div className="card">
+          <div className="image bg-yellow-200">This is imaginary image</div>
+          <div className="m-4">
+            <span className="font-bold text-gray-500">
+              Grilled Chicken Salad
+            </span>
+            <span className="block text-gray-500 text-sm">Recipe by Unkle</span>
+          </div>
+          <div className="badge">
+            <span>25 mins</span>
+          </div>
+        </div>
+        <div className="card">
+          <div className="image bg-blue-200">This is imaginary image</div>
+          <div className="m-4">
+            <span className="font-bold text-gray-500">
+              Grilled Chicken Salad
+            </span>
+            <span className="block text-gray-500 text-sm">Recipe by Unkle</span>
+          </div>
+          <div className="badge">
+            <span>25 mins</span>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default Orders
