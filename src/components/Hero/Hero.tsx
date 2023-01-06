b01745212ed9d5423e951e6d5e8851f473e1c02d
Nhi Nguyen <nhinguyen@Nhis-MacBook-Pro.local>
2023-01-06 22:36:16 +0700

    Made change to background color and text font size

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center">
-      <h1>Career Bliss</h1>
+    <div className="w-full h-screen flex flex-col items-center justify-center background">
+      <h1 className="brand-name">Career Bliss</h1>
       <p>Empower your career & academic with focus, balance and joy</p>
       <button>Contact Us</button>
     </div>
