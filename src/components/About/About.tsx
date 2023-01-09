e38b06542ed6f3517f724cb249809bc24ce0b015
Nhi Nguyen <nhinguyen@Nhis-MacBook-Pro.local>
2023-01-09 23:29:07 +0700

    VSKY-69 removed extra className from About component, added hover effect for button in Hero component, changed branch from Main to Develop

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,7 @@
 
 const About = () => {
   return (
-    <div className="h-screen text-center border">
+    <div className="h-screen text-center">
       {/* Please add HTML content as you go */}
       <h1>About section</h1>
     </div>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,8 +5,8 @@ const Hero = () => {
   return (
     <div className="w-full h-screen flex justify-center flex-col background">
       <div className="hero-section-content">
-        <h1 className="brand-name line-space shadow-effect">Career Bliss</h1>
-        <p className="hero-section-paragraph line-space shadow-effect">Empower your career & academic with focus, balance and joy</p>
+        <h1 className="brand-name pb-[15px] shadow-effect">Career Bliss</h1>
+        <p className="hero-section-paragraph pb-[15px] shadow-effect">Empower your career & academic with focus, balance and joy</p>
         <button className="hero-btn">Contact Us</button>
       </div>
     </div>
