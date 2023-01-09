7aac215781e2bbef9c955d0e72d511a6b57194cd
ANLEHOANG-THINKPAD2022 <an.lehoang1707@gmail.com>
2023-01-09 20:37:14 +0700

    VSKY-70: add new h1 demo

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,6 +4,7 @@ const About = () => {
     <div className="h-screen text-center">
       {/* Please add HTML content as you go */}
       <h1>About section</h1>
+      <h1>Hello, this is An demo for Git workflow</h1>
     </div>
   )
 }
