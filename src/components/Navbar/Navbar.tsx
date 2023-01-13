090fe29d302a191434535a10359487847eed53c8
Julie Lai <80876101+higherstates@users.noreply.github.com>
2023-01-13 21:07:09 +0700

    VSKY-68: Fix whitespace on page & add logo for Header (#20)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,13 +2,10 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between p-5 relative">
-      <div className="logo">Career Bliss</div>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="#about">About</Link>
-        <Link to="#contact">Contact</Link>
-      </div>
+    <div className="flex justify-between">
+      <Link to="/">Home</Link>
+      <Link to="#about">About</Link>
+      <Link to="#contact">Contact</Link>
     </div>
   )
 }
