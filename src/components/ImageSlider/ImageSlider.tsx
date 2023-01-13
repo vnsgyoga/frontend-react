df99a49fb3abb5fd3af03c00da76d54933824999
Lawrence Le <85017175+lawrence-le@users.noreply.github.com>
2023-01-13 18:43:58 +0700

    VSKY-71: [FE] Build the Testimonials section (#16)

---
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -0,0 +1,86 @@
+import classNames from "classnames"
+import { useEffect, useState } from "react"
+import "./ImageSlider.css"
+
+type Slide = {
+  imgUrl: string
+  script: string
+  author: string
+  position: string
+  company: string
+}
+
+type Props = { slides: Slide[] }
+
+const ImageSlider = ({ slides }: Props) => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  const [isClicked, setIsClicked] = useState<boolean>(false)
+
+  const handlePrevious = (e: React.MouseEvent<any>) => {
+    e.stopPropagation()
+    const isFirstSlide = currentIndex === 0
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1
+    setCurrentIndex(newIndex)
+    setIsClicked(true)
+  }
+
+  const handleNext = (e: React.MouseEvent<any>) => {
+    e.stopPropagation()
+    const isLastSlide = currentIndex === slides.length - 1
+    const newIndex = isLastSlide ? 0 : currentIndex + 1
+    setCurrentIndex(newIndex)
+    setIsClicked(true)
+  }
+
+  useEffect(() => {
+    if (isClicked) {
+      const resetIsClicked = setTimeout(() => {
+        setIsClicked(false)
+      }, 1200)
+
+      return () => clearTimeout(resetIsClicked)
+    }
+  }, [isClicked])
+
+  return (
+    <div className="w-[600px] md:w-[700px] lg:w-[1000px]  relative">
+      <div
+        className={classNames(
+          "w-full h-full flex flex-col items-center justify-center",
+          {
+            slides__animation: isClicked,
+          }
+        )}
+      >
+        <img
+          src={slides[currentIndex].imgUrl}
+          alt=""
+          className="w-16 h-16 md:w-24 md:h-24 lg:w-32 lg:h-32 object-cover rounded-full"
+        />
+        <p className="max-w-[280px] sm:max-w-xs md:max-w-lg lg:max-w-xl text-center pt-8 md:pt-10 lg:pt-12 pb-5 lg:pb-7 font-secondary text-regular md:text-h3 tracking-wide">
+          {slides[currentIndex].script}
+        </p>
+        <p className="font-secondary text-regular-sm md:text-h5 tracking-wide">
+          {slides[currentIndex].author} - {slides[currentIndex].position} at{" "}
+          {slides[currentIndex].company}
+        </p>
+      </div>
+      <button
+        onClick={handlePrevious}
+        disabled={isClicked}
+        className="absolute top-1/2 left-0 bg-white w-8 h-8 md:w-12 md:h-12 rounded-full flex items-center justify-center cursor-pointer border-none hover:bg-[#F4F4F4] duration-200"
+      >
+        ❮
+      </button>
+      <button
+        onClick={handleNext}
+        disabled={isClicked}
+        className="absolute top-1/2 right-0 bg-white w-8 h-8 md:w-12 md:h-12  rounded-full flex items-center justify-center cursor-pointer border-none hover:bg-[#F4F4F4] duration-200"
+      >
+        ❯
+      </button>
+    </div>
+  )
+}
+
+export default ImageSlider
--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -1,8 +1,36 @@
+import image1 from "../../assets/images/testimonials/author1.png"
+import image2 from "../../assets/images/testimonials/author2.png"
+import image3 from "../../assets/images/testimonials/author3.png"
+import ImageSlider from "../ImageSlider/ImageSlider"
+
 const Testimonials = () => {
+  const slides = [
+    {
+      imgUrl: image1,
+      script: `“Achieving a goal is often an incredible feeling. Once you've conquered something, it's natural to want to keep moving forward. Career Bliss has inspired me to become greater and found my own peace!”`,
+      author: "Christine Richer",
+      position: "CEO",
+      company: "Acme Co.",
+    },
+    {
+      imgUrl: image2,
+      script: `“I'm so grateful to Career Bliss. I had been feeling really lost in my career after my company's downsizing last year and thought I would never find a job that I liked and was satisfied with. But thanks to them, I was able to get a perfect position that was exactly what I wanted! And it all happened in a very short time! It's been an absolute blessing for me.”`,
+      author: "James Berk",
+      position: "Software Engineer",
+      company: "WeCode",
+    },
+    {
+      imgUrl: image3,
+      script: `“Career Bliss has been one of the best decisions I have made in my professional life. I wasn't sure what career path I wanted to take, so I came across Career Bliss and decided to give it a try. It was amazing! They helped me find my passion in life and gave me all the information on how to get there.”`,
+      author: "Diane Hampton",
+      position: "Project Manager",
+      company: "Solution Tech",
+    },
+  ]
+
   return (
-    <div className="h-screen text-center">
-      {/* Please add HTML content as you go */}
-      <h1>Testimonials section</h1>
+    <div className="min-h-[400px] md:min-h-[700px] bg-[#FFF8FC] flex items-center justify-center">
+      <ImageSlider slides={slides} />
     </div>
   )
 }
