e8ff029c5d4db4e4b9a1f38c452806f022638889
Nhi Nguyen <nhinguyen@Nhis-MacBook-Pro.local>
2023-01-26 16:56:09 +0700

    VSKY-68: Made Header responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,27 @@
 import { Link } from "react-router-dom"
+import { Component } from "react"
+import "./Navbar.css"
 
-const Navbar = () => {
+class Navbar extends Component {
+  state={ clicked : false };
+  handleOnclick = () => {
+    this.setState({clicked:!this.state.clicked})
+  }
+
+  render() {
   return (
     <div className="flex justify-between">
-      <Link to="/">Home</Link>
-      <Link to="#about">About</Link>
-      <Link to="#contact">Contact</Link>
+      <div id="nav-menu" className={this.state.clicked ? "#nav-menu active" : "#nav-menu"}>
+      <Link to="/" className="link">Home</Link>
+      <Link to="#about" className="link">About</Link>
+      <Link to="#contact" className="link">Contact</Link>
+      </div>
+      <div id="hamburger" onClick={this.handleOnclick}>
+        <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
     </div>
   )
+  }
 }
 
 export default Navbar
